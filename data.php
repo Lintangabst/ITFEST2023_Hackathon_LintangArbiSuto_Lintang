71d9b5f21f97a5bbf5639e58be5d2f123b95f2c9
Lintangabst <90893554+Lintangabst@users.noreply.github.com>
2023-07-16 09:45:20 +0700

    Update data.php

--- data.php
+++ data.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
   <style>
     .invoice-container {
       background-image: url('img/mood.png');
@@ -34,7 +35,7 @@
       }
 
       // Query untuk mengambil data terbaru dari tabel klien
-      $query = "SELECT nama, uname, order_id, transaction_status FROM klien ORDER BY id DESC LIMIT 1";
+      $query = "SELECT nama, uname, order_id, transaction_status, metode_pembayaran, biaya FROM klien ORDER BY id DESC LIMIT 1";
       $result = $conn->query($query);
 
       // Tampilkan data
@@ -44,6 +45,8 @@
         $uname = $row["uname"];
         $orderId = $row["order_id"];
         $transactionStatus = $row["transaction_status"];
+        $metodePembayaran = $row["metode_pembayaran"];
+        $biaya = $row["biaya"];
 
         echo '<div class="mb-4">';
         echo '<label class="block font-medium text-gray-700 font-bold">Nama:</label>';
@@ -52,9 +55,29 @@
         echo '<span class="block text-gray-500">' . $uname . '</span>';
         echo '<label class="block font-medium text-gray-700 font-bold">Order ID:</label>';
         echo '<span class="block text-gray-500">' . $orderId . '</span>';
+        echo '<label class="block font-medium text-gray-700 font-bold">Harus Di Bayar:</label>';
+        echo '<span class="block text-gray-500">' . $biaya . '</span>';
+        echo '<label class="block font-medium text-gray-700 font-bold">Rekening Tujuan:</label>';
+
+        // Tampilkan informasi rekening tujuan berdasarkan metode pembayaran
+        switch ($metodePembayaran) {
+          case 'BCA':
+            $rekeningTujuan = '7010489185';
+            break;
+          case 'OVO':
+          case 'DANA':
+          case 'SHOPEEPAY':
+            $rekeningTujuan = '081317157363';
+            break;
+          default:
+            $rekeningTujuan = '-';
+            break;
+        }
+
+        echo '<span class="block text-gray-500">' . $rekeningTujuan . '</span>';
         echo '<label class="block font-medium text-gray-700 font-bold">Status Transaksi:</label>';
 
-        // Ubah tampilan status berdasarkan nilai transaction_status
+        // Tampilkan informasi status transaksi berdasarkan nilai transaction_status
         switch ($transactionStatus) {
           case 1:
             echo '<span class="block text-red-500">Pembayaran Belum Dilakukan</span>';
@@ -78,6 +101,11 @@
       // Tutup koneksi
       $conn->close();
       ?>
+
+<a href="https://api.whatsapp.com/send?phone=081317157363" target="_blank" class="fixed bottom-4 right-4">
+  <i class="fas fa-question-circle text-4xl text-yellow-400"></i>
+</a>
+
 
     </div>
   </div>
